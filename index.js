fac4e752120a955cf2cdbfd7e0a57645fd651eb4
iamvaras <iamvaras@gmail.com>
2023-11-03 17:19:37 +0530

    Create app.post() in server.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const BrandName = require('./model')
 const app = express()
+app.use(express.json())
 
 
 mongoose.connect(`mongodb+srv://iamvarastesting:India789@cluster0.mstubpk.mongodb.net/?retryWrites=true&w=majority`)
@@ -11,6 +12,19 @@ mongoose.connect(`mongodb+srv://iamvarastesting:India789@cluster0.mstubpk.mongod
 app.get('/', (req, res)=>{
     res.send("Hello World")
 })
+
+app.post('/addbrands' , async(req, res) => {
+    const{brandname} = req.body;
+    try{
+        const newData = new BrandName({brandname})
+        await newData.save();
+        return res.json(await BrandName.find())
+    } catch(err){
+        console.log(err.message)
+    }
+})
+
+
 
 app.listen('3000', ()=>{
     console.log("Server Running")
