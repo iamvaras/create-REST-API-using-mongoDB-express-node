66762193ac0d17afbb5172fe41f30c0ca5a30ff2
iamvaras <iamvaras@gmail.com>
2023-11-03 15:43:07 +0530

    Connect MongoDB using mongoose from the local

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+
+mongoose.connect(`mongodb+srv://iamvarastesting:India789@cluster0.mstubpk.mongodb.net/?retryWrites=true&w=majority`)
+    .then(()=> console.log('DB Connected !!!'))
+    .catch( err => console.log(err))
 
 app.get('/', (req, res)=>{
     res.send("Hello World")
